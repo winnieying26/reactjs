0d31407e94e5a8ee870e096a4409cd82033c934d
winnieying26 <winniequan55@gmail.com>
2024-12-10 18:20:03 -0500

    update array in state

---
+++ day11/components/Array.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Array() {
+  const [foods, setFoods] = useState(["Apple", "Mango", "Banana"]);
+
+  function handleAddFood(){
+    const newFood = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value = "" // reset 
+    setFoods(f =>[...f, newFood]);
+  }
+  function handleRemoveFood(index) {
+    setFoods(foods.filter((_, i)=> i !== index)); // _ will be ignored
+  }
+  return (
+  <div>
+    <h2>List Of Food</h2>
+    <ul>
+        {foods.map((food, index)=>
+        <li key = {index} onClick={()=> handleRemoveFood(index)}>
+            {food}
+            </li>)}
+    </ul>
+    <input type="text" id="foodInput" placeholder="Enter food name" />
+    <button onClick={handleAddFood}>Add Food</button>
+  </div>)
+}
+
+export default Array;
